4b1ba10286a3a89cd69ca9c9aa99e1e95ae7f759
Abdulhameed <abdulhameedgiwa@gmail.com>
2024-06-22 14:30:28 +0100

    Attempt separating api_view from normal view

--- SalatTracker/urls.py
+++ SalatTracker/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PrayerTimeViewSet, PrayerTimeFetch, DailyPrayerViewSet
+from .api_views import PrayerTimeViewSet, PrayerTimeFetch, DailyPrayerViewSet
 
 router = DefaultRouter()
 router.register(r'prayer-times', PrayerTimeViewSet)
