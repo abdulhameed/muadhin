ad5a24c858cb2904b537e3d5d8c3462a9970f2b8
Abdul <abdulhameedgiwa@gmail.com>
2025-08-04 21:20:54 +0100

    update urls.py

--- muadhin/urls.py
+++ muadhin/urls.py
@@ -48,6 +48,7 @@ urlpatterns = [
     path('api/', include('SalatTracker.urls')),
     path('api/', include('users.urls')),
     path('api/subscriptions/', include('subscriptions.urls')),
+    path('api/communications/', include('communications.urls')),
     # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
